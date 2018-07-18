da1c0a2e3c235db5c722124a34297c8acbbbc4c4
Jlack6 <jlack6@gmail.com>
2018-07-18 19:32:00 -0400

    Update config.php

--- config.php
+++ config.php
@@ -2,7 +2,7 @@
 
 define('DB_SERVER', 'xo3.x10hosting.com');
 define('DB_USERNAME', 'pract147');
-define('DB_PASSWORD', 'Lambo1896');
+define('DB_PASSWORD', '');
 define('DB_NAME', 'pract147_pulsd');
  
 $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
